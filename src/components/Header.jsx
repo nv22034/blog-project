15b2af6ca6b3acf0807e80893e0673bda99b2eeb
Mohamed Elansary <132562932+nv22034@users.noreply.github.com>
2024-02-28 05:41:47 +0000

    3 - creating header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,30 @@ import { images } from '../constants'
 const Header = () => {
   return (
     <section>
-        <header>
+        <header className='container mx-auto px-5 flex justify-between'>
             <div>
-                <img src={images.logo} alt="Logo" />
+                <img src={images.Logo} alt="Logo" />
             </div>
-            <div></div>
+            <div className='flex gap-x-9'>
+              <ul className='flex gap-x-5'>
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="/">Articles</a>
+                </li>
+                <li>
+                  <a href="/">Pages</a>
+                </li>
+                <li>
+                  <a href="/">Pricing</a>
+                </li>
+                <li>
+                  <a href="/">Faq</a>
+                </li>
+              </ul>
+              <button>Sign in</button>
+            </div>
         </header>
     </section>
   )
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import MainLayout from '../components/MainLayout'
+import React from "react"
+import MainLayout from "../components/MainLayout"
 
 const Homepage = () => {
   return <MainLayout></MainLayout>
