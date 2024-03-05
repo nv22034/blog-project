50ed6ea45e394a0742d1ad31c5a78fb7f564316c
Mohamed Elansary <132562932+nv22034@users.noreply.github.com>
2024-03-05 07:32:28 +0000

    4 - Responsive Header 23:37

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,52 +1,86 @@
-import React from 'react'
-import { images } from '../constants'
+import React, { useState } from 'react';
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
-const navItemsInfo =[
-  {name: "Home"},
-  {name: "Articles"},
-  {name: "Pages"},
-  {name: "Faq"},
-  {name: "Home"},
-]
+import { images } from '../constants';
 
-const NavItem = ({name}) => {
+const navItemsInfo = [
+  { name: 'Home', type: "Link" },
+  { name: 'Articles', type: "Link" },
+  { name: 'Pages', type: "dropdown", items: ['About us', 'Contact us'] },
+  { name: 'Pricing', type: "Link" },
+  { name: 'Faq', type: "Link" },
+];
+
+const NavItem = ({ item }) => {
   return (
     <li className='relative group'>
-                  <a href="/" className='px-4 py-2'>{name}</a>
-                  <span className='text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>
-                  /
-                  </span>
-                </li>
+      {item.type === "Link" ? (
+        <>
+          <a href='/' className='px-4 py-2'>
+            {item.name}
+          </a>
+          <span className='text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>
+            /
+          </span>
+        </>
+      ) : (
+        <>
+          <a href='/' className='px-4 py-2'>
+            {item.name}
+          </a>
+          <div className='hidden transision-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max'></div>
+        </>
+      )}
+    </li>
   );
-  
 };
 
 function Header() {
-const [natIsVisible, setNatIsVisible] = useState(false);
+  const [navIsVisible, setNavIsVisible] = useState(false);
 
-const navVisibility = () => {
-  setNatIsVisible((curState) => {
-    return !curState;
-  )}
-}
+  const toggleNavVisibilityHandler = () => {
+    setNavIsVisible((curState) => {
+      return !curState;
+    });
+  };
 
   return (
     <section>
       <header className='container mx-auto px-5 flex justify-between py-4 items-center'>
         <div>
-          <img className='w-16' src={images.Logo} alt="Logo" />
+          <img className='w-16' src={images.Logo} alt='Logo' />
         </div>
-        <div className={`'${natIsVisible ? "right-0" : "-right-full" }flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static  gap-x-9 items-center'`}>
-          <ul className='flex gap-x-2 font-semibold'>
+        <div className='lg:hidden z-50'>
+          {navIsVisible ? (
+            <AiOutlineClose
+              className='w-6 h-6'
+              onClick={toggleNavVisibilityHandler}
+            />
+          ) : (
+            <AiOutlineMenu
+              className='w-6 h-6'
+              onClick={toggleNavVisibilityHandler}
+            />
+          )}
+        </div>
+        <div
+          className={`${
+            navIsVisible ? 'right-0' : '-right-full'
+          } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
+        >
+          <ul className='text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold'>
             {navItemsInfo.map((item) => (
-              <NavItem key={item.name} name={item.name} />
+              <NavItem key={item.name} item={item} />
             ))}
           </ul>
-          <button className='border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300'>Sign in</button>
+          <button className='mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300'>
+            Sign in
+          </button>
         </div>
       </header>
     </section>
-  )
+  );
 }
 
-export default Header
+export default Header;
