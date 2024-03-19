1d6050e5bd6935787e9d6882e56ef4a44afb2721
Mohamed Elansary <132562932+nv22034@users.noreply.github.com>
2024-03-19 07:31:49 +0000

    6 - Atricle crad component

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -3,8 +3,11 @@ import ArticleCard from '../../../components/ArticleCard'
 
 const Articles = () => {
   return (
-    <section className='container mx-auto flex-wrap md:gap-x-5 gap-y-5 px-5 py-10'>
-      <ArticleCard className="w-full" />
+    <section className='container mx-auto px-5 py-10'>
+      <div className='flex flex-wrap md:gap-x-5 gap-y-5'>
+      <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
+      <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
+      </div>
     </section>
   )
 }
