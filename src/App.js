f8af96c973394ced189c2d43e447f41449c938ae
Mohamed Elansary <132562932+nv22034@users.noreply.github.com>
2024-03-28 08:01:04 +0000

    10 - Comments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 
-import './App.css';
-import Homepage from './pages/home/Homepage';
+import "./App.css";
+import Homepage from "./pages/home/Homepage";
+import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 
 function App() {
   return (
     <div className="App font-opensans">
-       <Homepage />
+      <Routes>
+        <Route index path="/" element={<Homepage />} />
+        <Route path="/blog/:id" element={<ArticleDetailPage />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
